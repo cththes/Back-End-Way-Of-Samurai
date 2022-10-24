245360fb29bfe5bb3b419797ae979c4f4c09c533
cth <cththes@gmail.com>
2022-10-24 15:10:06 +0600

    add fs, async, 06 samurai back-end

--- server.js
+++ server.js
@@ -1,24 +1,46 @@
 const http = require('http')
+const fs = require('fs')
 
-let requestCount = 0
+const delay = (ms) => {
+   return new Promise((resolve, reject) => {
+      setTimeout(() =>{
+         resolve()
+      }, ms)
+   })
+}
 
-const server = http.createServer((requrest, response) => {
-   requestCount++
+const readFile = (path) => {
+   return new Promise((res, rej) => {
+      fs.readFile(path, (err, data) => {
+         if (err) rej(err)
+         else res(data)
+      })
+   })
+}
 
+const server = http.createServer(async (requrest, response) => {
    switch(requrest.url){
-      case '/students':
-         response.write('STUDENTS')
-         break;
-      case '/':
-      case '/courses':
-         response.write('FRONT + BACK')
-         break;
+      case '/home': {
+         try {
+           const data = await readFile('pages/about.html')
+           response.write(data)
+           response.end()
+         } catch (err) {
+            response.write('something wrong, 500')
+            response.end()
+         }
+        break;
+      }
+      case '/about':{
+         await delay(3000)
+         response.write('about')
+         response.end()
+         break
+      }
       default:
          response.write('404 not found')
-
+         response.end()
    }
-   response.write('IT-KAMASUTRA' + requestCount)
-   response.end()
 })
 
 server.listen(3003)
